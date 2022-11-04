6e166b9911a7523fb2b5eb881b20a38b733b34ea
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2022-11-04 11:32:38 -0300

    first-commit

---
+++ OurBankApi/src/main/java/com/OurBank/OurBankApi/OurBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.OurBank.OurBankApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OurBankApiApplication.class, args);
+	}
+
+}
---
+++ OurBankApi/src/test/java/com/OurBank/OurBankApi/OurBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.OurBank.OurBankApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
