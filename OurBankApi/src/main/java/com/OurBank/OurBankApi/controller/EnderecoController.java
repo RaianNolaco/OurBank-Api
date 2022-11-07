88cdec0ff63665297005e3d7988ec33ac780ef05
Mariana <mariana0026souza@gmail.com>
2022-11-06 22:22:43 -0300

    + rota POST endereço

--- OurBankApi/src/main/java/com/OurBank/OurBankApi/controller/EnderecoController.java
+++ OurBankApi/src/main/java/com/OurBank/OurBankApi/controller/EnderecoController.java
@@ -5,6 +5,8 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,6 +28,11 @@ public class EnderecoController {
     public ResponseEntity<List<EnderecoModel>> ListarEnderecos(){
         List<EnderecoModel> lista = enderecoService.listarEnderecos();
         return ResponseEntity.status(200).body(lista);
+    }
+
+    @PostMapping
+    public ResponseEntity<EnderecoModel> criarEndereco (@RequestBody EnderecoModel endereco) {
+        return ResponseEntity.status(201).body(enderecoService.addEndereco(endereco));
     }
     
 }
