1fd99ff48d4f981247e145e98fd2ac97a2089c3b
Nathalia Da Rocha Santos <nathaliarochasantos9@gmail.com>
2022-11-13 14:50:05 -0300

    Criação de controller

---
+++ OurBankApi/src/main/java/com/OurBank/OurBankApi/repository/ICliente.java
@@ -0,0 +1,19 @@
+package com.OurBank.OurBankApi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import com.OurBank.OurBankApi.model.ClienteModel;
+
+
+public interface ICliente extends JpaRepository<ClienteModel,Integer>, JpaSpecificationExecutor<ClienteModel> {
+
+    // esse é um selec criado para podermos para buscarmos o cliente via cpd 
+    public final static String BUSCAR_POR_CPF = "SELECT * FROM tb_cliente WHERE cpf = :cpf";
+
+    // aqui no metodo nos expecificamos para o JPA que queremos execultar a varivel BUSCAR_POR_CPF 
+    @Query(value = BUSCAR_POR_CPF,nativeQuery = true)
+    public ClienteModel findByCpf(@Param("cpf")final String cpf);
+}
---
+++ OurBankApi/src/main/java/com/OurBank/OurBankApi/service/ClienteService.java
@@ -0,0 +1,57 @@
+package com.OurBank.OurBankApi.service;
+
+import org.springframework.stereotype.Service;
+
+import com.OurBank.OurBankApi.model.ClienteModel;
+import com.OurBank.OurBankApi.repository.ICliente;
+
+import java.util.List;
+
+@Service
+public class ClienteService {
+
+    private ICliente repositoryCliente;
+
+    public ClienteService(ICliente repositoryCliente){
+        this.repositoryCliente = repositoryCliente;
+    }
+
+    //listando todos os clientes 
+    public List<ClienteModel> listarTodosUsuarios(){
+        List<ClienteModel> listaClientes = repositoryCliente.findAll();
+        return listaClientes;
+    }
+
+    //adicionando um cliente novo
+    public ClienteModel cadastrarCliente(ClienteModel cliente){
+        ClienteModel novoCliente = repositoryCliente.save(cliente);
+        return novoCliente;
+    }
+
+    //editando os dados de um cliente
+    public ClienteModel editarCliente(ClienteModel cliente){
+        ClienteModel editarCliente = repositoryCliente.save(cliente);
+        return editarCliente;
+    }
+
+    //buscando um cliente via Id
+    public ClienteModel buscarCliente(Integer id){
+        ClienteModel cliente = repositoryCliente.findById(id).get();
+        return cliente;
+    }
+
+    //Buscando um cliente vida cpf
+    public ClienteModel buscarClientePorCpf(String cpf){
+       ClienteModel cliente = repositoryCliente.findByCpf(cpf);
+        return cliente;
+    }
+
+    //deletando um cliente
+    public boolean deletarCliente(Integer id) {
+        repositoryCliente.deleteById(id);
+        return true;
+    }
+
+
+
+}
