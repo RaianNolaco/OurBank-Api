9a0cad187b8104d1bfcccc2bc1625178f57f0415
Raian Medeiros Nolaço <raiannolaco@gmail.com>
2022-11-13 15:27:31 -0300

    <<Corrigindo>>

--- OurBankApi/src/main/java/com/OurBank/OurBankApi/repository/IConta.java
+++
@@ -1,9 +0,0 @@
-package com.OurBank.OurBankApi.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.OurBank.OurBankApi.model.ContaModel;
-
-public interface IConta extends JpaRepository<ContaModel,Integer> {
-    
-}
